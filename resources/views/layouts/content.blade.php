c83de4034bc1113662aa100909d8d745873a6087
macfa <kombo6768@gmail.com>
2021-07-03 14:34:54 +0900

    210703 Macfa ]  modify script tag into head

--- resources/views/layouts/content.blade.php
+++ resources/views/layouts/content.blade.php
@@ -68,7 +68,7 @@
         </article>
     </div>
 </section>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
 <script>
     $('#main .tab li').click(function(event){
         $('#main .tab li').removeClass('on');
