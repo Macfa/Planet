ff18b187c042f830bd56ce35f34dfdce7a1e1081
macfa <kombo6768@gmail.com>
2021-07-31 22:40:22 +0900

    ...

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -73,11 +73,11 @@ class LoginController extends Controller
 
     public function handleProviderCallback( $driver )
     {
-        $user = Socialite::driver($driver)->stateless()->user();
-        dd($user);
+//        $user = Socialite::driver($driver)->stateless()->user();
+//        dd($user);
         try {
 //            $user = Socialite::driver($driver)->stateless()->user();
-            $user = Socialite::driver($driver)->user();
+            $user = Socialite::driver($driver)->stateless()->user();
 //            return dd($user);
         } catch (Exception $e) {
             return $this->sendFailedResponse($e->getMessage());
