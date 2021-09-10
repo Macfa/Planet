121bb7c6df3706452608292861fe167212fe9bc9
macfa <kombo6768@gmail.com>
2021-09-10 22:41:54 +0900

    210910 Macfa ] 댓글 로직 수정
    전반적인 함수 수정
    추후에 공통 코드를 따로 빼낼 예정

--- resources/views/layouts/comment.blade.php
+++ resources/views/layouts/comment.blade.php
@@ -16,7 +16,7 @@
                 </div>
 
                 <div class="write-btn">
-                    <button type="submit" onclick="addComment();">등록</button>
+                    <button type="submit" onclick="checkCommentTypeToProcess('add');">등록</button>
                 </div>
             </div>
         </div>
@@ -44,7 +44,7 @@
 
                         @if(auth()->id()==$comment->userID)
                             <div class="ml-20 comment-modi-form">
-                                <button onclick="editComment({{ $comment->id }})">
+                                <button onclick="checkCommentTypeToAddForm('edit', {{ $comment->id }})">
                                     <div class="function-text">
                                         <p>수정</p>
                                     </div>
@@ -61,7 +61,7 @@
                     <div class="comment-info">
                         <ul>
                             <li>스탬프</li>
-                            <li class="clickable" onclick="replyCommentForm({{ $comment->group }}, {{ $post->id }}, {{ $comment->id }});">댓글</li>
+                            <li class="clickable" onclick="checkCommentTypeToAddForm('add', {{ $comment->id }});">댓글</li>
                             <li class="clickable">
                                 <img onclick="voteLikeInComment({{ $comment->id }}, 1)" id="comment-{{ $comment->id }}-upvote" class="image-sm" alt=""
                                      @if($comment->existCommentLike == 1)
@@ -91,51 +91,8 @@
                         {{ $comment->content }}
                     </p>
                 </div>
-                <!-- 답글 작성 폼 -->
-
             </div>
         </div>
-
-        {{--        @else--}}
-
-        {{--        <div class="comment-list comment-{{ $comment->id }}">--}}
-        {{--            <div class="comment-item">--}}
-        {{--                <div class="reply-list">--}}
-        {{--                    <div class="reply-item">--}}
-        {{--                        <div class="reply-top">--}}
-        {{--                            <div class="write-info">--}}
-        {{--                                <img src="{{ asset('image/square-big.png') }}" alt="닉네임" />--}}
-        {{--                                <h5 class="nickname">닉네임</h5>--}}
-        {{--                                <p>{{ $comment->created_at->diffForHumans() }}</p>--}}
-        {{--                            </div>--}}
-
-        {{--                            <div class="reply-info">--}}
-        {{--                                <ul>--}}
-        {{--                                    <li>스탬프</li>--}}
-        {{--                                    <li onclick="reply({{ $comment->group }}, {{ $post->id }}, {{ $comment->id }});">댓글</li>--}}
-        {{--                                    <li>--}}
-        {{--                                        <img onclick="commentLikeVote({{ $comment->id }}, 1)" src="{{ asset('image/square-small.png') }}" alt="" />--}}
-        {{--                                        <span class="comment-like">{{ $comment->like }}</span>--}}
-        {{--                                    </li>--}}
-        {{--                                    <li>--}}
-        {{--                                        <img onclick="commentLikeVote({{ $comment->id }}, -1)" src="{{ asset('image/square-small.png') }}" alt="" />--}}
-        {{--                                    </li>--}}
-        {{--                                </ul>--}}
-        {{--                            </div>--}}
-        {{--                        </div>--}}
-
-        {{--                        <div class="reply-cont">--}}
-        {{--                            <p>--}}
-        {{--                                {{ $comment->content }}--}}
-        {{--                            </p>--}}
-        {{--                        </div>--}}
-        {{--                        <!-- 답글 작성 폼 -->--}}
-
-        {{--                    </div>--}}
-        {{--                </div>--}}
-        {{--            </div>--}}
-        {{--        </div>--}}
-        {{--        @endif--}}
     @empty
         <p>No users</p>
 @endforelse
@@ -152,6 +109,160 @@
         };
     })();
 
+    // add : 댓글, 대댓글   |   edit : 수정
+    function checkCommentTypeToAddForm(commentType, commentID) {
+        if(commentType == "edit") {
+            checkExistEditForm(commentID);
+        } else if(commentType == "add") {
+            checkExistAddForm(commentID);
+        }
+    }
+    function checkExistEditForm(commentID) {
+        var el = $(".reply-form");
+
+        if(el.length >= 1) {
+            // 이미 열려있는 폼의 아이디를 가져온다
+            var existCommentID = $(".reply-form input[name='id']").val();
+            toggleEditForm('hide', existCommentID);
+
+            // 새로 수정하고하는 댓글의 수정 폼을 연다
+            toggleEditForm('show', commentID);
+        } else {
+            toggleEditForm('show', commentID);
+        }
+    }
+    function toggleEditForm(status, commentID) {
+        var el = $(`.comment-info, .comment-modi-form, .comment-cont`, `.comment-${commentID}`);
+        if(status == "show") {
+            el.hide();
+
+            var templateValues = {
+                "commentID": commentID,
+                "form": "edit"
+            };
+
+            $("#replyWriteForm").tmpl(templateValues).appendTo(`.comment-${commentID} .comment-item`);
+        } else if(status == "hide") {
+            $(`.comment-${commentID} .reply-form`).remove();
+            el.show();
+        }
+    }
+    function checkExistAddForm(commentID) {
+        var el = $(".reply-form");
+
+        if(el.length >= 1) {
+            // 이미 열려있는 폼의 아이디를 가져온다
+            var existCommentID = $(".reply-form input[name='id']").val();
+            toggleAddForm('hide', existCommentID);
+
+            // 새로 수정하고하는 댓글의 수정 폼을 연다
+            toggleAddForm('show', commentID);
+        } else {
+            toggleAddForm('show', commentID);
+        }
+    }
+    function toggleAddForm(status, commentID) {
+        if(status == "show") {
+            var templateValues = {
+                "commentID": commentID,
+                "form": "add"
+            };
+
+            $("#replyWriteForm").tmpl(templateValues).appendTo(`.comment-${commentID} .comment-item`);
+        } else if(status == "hide") {
+            $(`.comment-${commentID} .reply-form`).remove();
+        }
+    }
+    function cancleForm(form, commentID) {
+        if(form == "edit") {
+            toggleEditForm("hide", commentID);
+        } else if(form == "add") {
+            toggleAddForm("hide", commentID);
+        }
+    }
+    function checkCommentTypeToProcess(form, commentID) {
+        if(form == "add") {
+            addComment(form, commentID);
+        } else if(form == "edit") {
+            addComment(form, commentID);
+        }
+    }
+    function addComment(form, commentID) {
+        // init variables
+        var data = "";
+        var type = "";
+        var url = "";
+
+        // 댓글, 대댓글 구분
+        if(commentID) {
+            data = $(`.comment-${commentID} .reply-form form`).serialize();
+        } else {
+            data = $("#comment-form").serialize();
+        }
+
+        // 생성, 수정 구분
+        if(form == "add") {
+            type = "post";
+            url = "/comment";
+        } else if(form == "edit") {
+            type = "put";
+            url = "/comment/" + commentID;
+        }
+
+        delay(function() {
+            $.ajax({
+                url: url,
+                data: data,
+                type: type,
+                success: function(data) {
+                    if(form == "edit") { // 기존 댓글 업데이트 로직
+                        // alert(commentID);
+                        $(`.comment-${commentID} .comment-cont p`).html(data.content);
+                        toggleEditForm('hide', commentID);
+                    } else if(form == "add") { // 신규 댓글 생성 로직
+                        var templateValues = {
+                            "id": data.id,
+                            "depth": data.depth*44,
+                            "updated_at_modi": data.updated_at_modi,
+                            "group": data.group,
+                            "content": data.content,
+                            "sumOfVotes": data.sumOfVotes,
+                            "postID": data.postID,
+                            "avatar": data.user.avatar,
+                            "commentCount": data.commentCount,
+                            "name": data.user.name
+                        };
+
+                        if(data.commentCount < 2) { // 첫번째 글일 때, 댓글보기 헤더 추가
+                            $("#replyForm").tmpl(templateValues).insertAfter('.comment-write-form');
+                        } else { // 첫번쨰 글이 아닌 경우 그냥 추가
+                            if(commentID) { // 대댓글
+                                $("#replyForm").tmpl(templateValues).insertAfter(`.comment-${commentID}`);
+                            } else { // 댓글
+                                $("#replyForm").tmpl(templateValues).insertAfter(".section-title");
+                            }
+                        }
+                        if(commentID) {
+                            cancleForm('add', commentID);
+                        } else {
+                            $("#comment-form #comment_text").val('');
+                        }
+                    }
+
+
+
+                },
+                error: function(err) {
+                    if(err.responseJSON.reason == 'login') {
+                        alert("로그인이 필요한 기능입니다.");
+                    } else {
+                        alert("댓글이 저장되지않았습니다\n관리자에게 문의해주세요.");
+                    }
+                    console.log(err);
+                }
+            })
+        }, 300);
+    }
     function deleteComment(id) {
         if(confirm('삭제하시겠습니까 ?')) {
             $.ajax({
@@ -172,145 +283,6 @@
             })
         }
         return false;
-    }
-
-    function editComment(commentID) {
-        var el = ".comment-"+commentID+" .comment-item";
-        var el_header = el + ' > .comment-top .comment-modi-form, '+ el + ' > .comment-top .comment-info';
-        var el_content = el + ' > .comment-cont';
-
-        // if($(temp_el).length > 0) {
-        //     cancleReply(commentID);
-        //     return false;
-        // }
-        var valueList = {
-            "commentID": commentID
-        };
-        $(el_header).hide();
-        $(el_content).hide();
-        $("#editForm").tmpl(valueList).appendTo(el);
-    }
-
-    function cancleEdit(commentID) {
-        var el = ".comment-"+commentID+" .comment-item";
-        var el_header = el + ' > .comment-top .comment-modi-form, '+ el + ' > .comment-top .comment-info';
-        var el_content = el + ' > .comment-cont';
-        var el_edit = ".comment-"+commentID+" .comment-item .reply-form";
-        $(el_edit).remove();
-        $(el_header).show();
-        $(el_content).show();
-    }
-    function updateComment(commentID) {
-        var obj = "#comment-edit-form-" + commentID;
-        var data = $(obj).serialize();
-
-        delay(function() {
-            $.ajax({
-                url: "/comment/"+commentID,
-                data: data,
-                type: "PUT",
-                success: function(data) {
-                    var valueList = {
-                        "id": data.id,
-                        "depth": data.depth*44,
-                        "updated_at_modi": data.updated_at_modi,
-                        "group": data.group,
-                        "content": data.content,
-                        "sumOfVotes": data.sumOfVotes,
-                        "postID": data.postID,
-                        "avatar": data.user.avatar,
-                        "commentCount": data.commentCount,
-                        "name": data.user.name
-                    };
-                    if(data.commentCount < 2) { // 첫번째 글일 때, 댓글보기 헤더 추가
-                        $("#replyForm").tmpl(valueList).insertAfter('.comment-write-form');
-                    } else { // 첫번쨰 글이 아닌 경우 그냥 추가
-                        $("#replyForm").tmpl(valueList).insertAfter(el);
-                    }
-
-                    $('.commentCount').html(data.commentCount);
-                    if(commentID) {
-                        cancleReply(commentID);
-                    } else {
-                        $("#comment-form #comment_text").val('');
-                    }
-                },
-                error: function(err) {
-                    if(err.responseJSON.reason == 'login') {
-                        alert("로그인이 필요한 기능입니다.");
-                    } else {
-                        alert("댓글이 저장되지않았습니다\n관리자에게 문의해주세요.");
-                    }
-                    console.log(err);
-                }
-            })
-        }, 300);
-    }
-
-    function addComment(commentID) {
-        var obj = '';
-        var el = '';
-        if(commentID) { // 대댓글
-            obj = "#comment-form-" + commentID;
-            el = ".comment-" + commentID;
-        } else { // 댓글
-            obj = "#comment-form";
-            el = ".section-title";
-        }
-
-        var dataArray = $(obj).serializeArray(),
-            data = {};
-        $(dataArray).each(function(i, field){
-            data[field.name] = field.value;
-        });
-
-        if(data['content'] === "" ) { // 0 : postID, 1 : content
-            data['content'] = "내용이 없습니다";
-        }
-
-        // console.log(data);return;
-
-        delay(function() {
-            $.ajax({
-                url: "/comment",
-                data: data,
-                type: "post",
-                success: function(data) {
-                    var valueList = {
-                        "id": data.id,
-                        "depth": data.depth*44,
-                        "updated_at_modi": data.updated_at_modi,
-                        "group": data.group,
-                        "content": data.content,
-                        "sumOfVotes": data.sumOfVotes,
-                        "postID": data.postID,
-                        "avatar": data.user.avatar,
-                        "commentCount": data.commentCount,
-                        "name": data.user.name
-                    };
-                    if(data.commentCount < 2) { // 첫번째 글일 때, 댓글보기 헤더 추가
-                        $("#replyForm").tmpl(valueList).insertAfter('.comment-write-form');
-                    } else { // 첫번쨰 글이 아닌 경우 그냥 추가
-                        $("#replyForm").tmpl(valueList).insertAfter(el);
-                    }
-
-                    $('.commentCount').html(data.commentCount);
-                    if(commentID) {
-                        cancleReply(commentID);
-                    } else {
-                        $("#comment-form #comment_text").val('');
-                    }
-                },
-                error: function(err) {
-                    if(err.responseJSON.reason == 'login') {
-                        alert("로그인이 필요한 기능입니다.");
-                    } else {
-                        alert("댓글이 저장되지않았습니다\n관리자에게 문의해주세요.");
-                    }
-                    console.log(err);
-                }
-            })
-        }, 300);
     }
     function voteLikeInComment(id, vote, obj) {
         $.ajax({
@@ -337,56 +309,7 @@
             $("#comment-"+id+"-downvote").attr("src", "{{ asset('image/downvote_c.png') }}");
         }
     }
-    function replyCommentForm(group, postID, commentID) {
-        var el = ".comment-"+commentID+" .comment-item";
-        var temp_el = el + ' .reply-form';
 
-        if($(temp_el).length > 0) {
-            cancleReply(commentID);
-            return false;
-        }
-        var hasForm = hasReplyCommentForm();
-        var valueList = {
-            "group": group,
-            "postID": postID,
-            "commentID": commentID
-        };
-        $("#replyWriteForm").tmpl(valueList).appendTo(el);
-    }
-    function cancleReply(commentID) {
-        var el = ".comment-"+commentID+" .comment-item .reply-form";
-        $(el).remove();
-    }
-    function hasReplyCommentForm() {
-        var checkExistForm = $(".reply-form").length;
-
-        if(checkExistForm >= 1) {
-            $(".reply-form").remove();
-            // return true;
-        }
-    }
-</script>
-<script id="editForm" type="text/x-jquery-tmpl">
-<div class="reply-form">
-    <form method="post" onSubmit="return false;" id="comment-edit-form-${commentID}">
-    <div class="reply-input">
-        <textarea
-        name="content"
-        id="reply_text"
-        ></textarea>
-
-        <div class="form-btn">
-            <div class="reset-btn">
-                <button onclick="cancleEdit(${commentID})" type="reset">취소</button>
-            </div>
-
-            <div class="write-btn">
-                <button type="submit" onclick="updateComment(${commentID}, this);">등록</button>
-            </div>
-        </div>
-    </div>
-</form>
-</div>
 </script>
 <script id="replyForm" type="text/x-jquery-tmpl">
 @{{if commentCount < 2}}
@@ -403,7 +326,7 @@
                     <h5 class="nickname">${name}</h5>
                     <p>${updated_at_modi}</p>
                     <div class="ml-20 comment-modi-form">
-                        <button onclick="editComment(${id})">
+                        <button onclick="checkCommentTypeToAddForm('edit', ${id})">
                             <div class="function-text">
                                 <p>수정</p>
                             </div>
@@ -419,7 +342,7 @@
                 <div class="comment-info">
                     <ul>
                         <li>스탬프</li>
-                        <li onclick="replyCommentForm(${group}, ${postID}, ${id});">댓글</li>
+                        <li onclick="checkCommentTypeToAddForm('add', ${id});">댓글</li>
                         <li>
                             <img onclick="voteLikeInComment(${id}, 1)" id="comment-${$id}-upvote" class="image-sm" alt="" src="{{ asset('image/upvote.png') }}" />
                         </li>
@@ -443,8 +366,6 @@
 <script id="replyWriteForm" type="text/x-jquery-tmpl">
 <div class="reply-form">
     <form method="post" onSubmit="return false;" id="comment-form-${commentID}">
-    <input type="hidden" name="group" value="${group}">
-    <input type="hidden" name="postID" value="${postID}">
     <input type="hidden" name="id" value="${commentID}">
     <div class="reply-input">
         <textarea
@@ -454,11 +375,11 @@
 
         <div class="form-btn">
             <div class="reset-btn">
-                <button onclick="cancleReply(${commentID})" type="reset">취소</button>
+                <button onclick="cancleForm('${form}', ${commentID})" type="reset">취소</button>
             </div>
 
             <div class="write-btn">
-                <button type="submit" onclick="addComment(${commentID});">등록</button>
+                <button type="submit" onclick="checkCommentTypeToProcess('${form}', ${commentID});">등록</button>
             </div>
         </div>
     </div>
