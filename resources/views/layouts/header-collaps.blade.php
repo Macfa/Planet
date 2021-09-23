756865a9539cd46d721bd66377309f64c2f2924e
macfa <kombo6768@gmail.com>
2021-09-23 21:27:17 +0900

    1

--- resources/views/layouts/header-collaps.blade.php
+++ resources/views/layouts/header-collaps.blade.php
@@ -17,11 +17,13 @@
     </div>
 <div class="collapse" id="header-noti" style="position:absolute; right:2%; top:6.5%; width:15%">
     <div class="list-group" style="border-radius: 1rem;">
-        @forelse(auth()->user()->notifications as $notification)
-            <a href="" class="list-group-item list-group-item-action">{{ $notification->data['msg'] }}</a>
-        @empty
-            <a href="" class="list-group-item list-group-item-action">알림이 없습니다.</a>
-        @endforelse
+        @if(auth()->user()->notifications)
+            @forelse(auth()->user()->notifications as $notification)
+                <a href="" class="list-group-item list-group-item-action">{{ $notification->data['msg'] }}</a>
+            @empty
+                <a href="" class="list-group-item list-group-item-action">알림이 없습니다.</a>
+            @endforelse
+        @endif
     </div>
 </div>
 {{--@endsection--}}
