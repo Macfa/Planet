968c8755a80c6f4cf9e1121d03255b9deef35d74
Macfa <kombo6768@gmail.com>
2024-04-09 15:15:40 +0900

    24 / 04 /. 9 ] 핸들링 테스트

---
+++ app/Exceptions/CustomHandler.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
+
+class CustomHandler extends ExceptionHandler
+{
+}
